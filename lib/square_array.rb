5df03381674c210d8892fba602bb67e402782d66
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-23 19:01:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
+  newarray=[]
   counter=0
 while counter < array.length do
   num = array[counter]
   num**2
-  
-
-  
+  counter+=1
+newarray.push(num)
+return newarray
 end
